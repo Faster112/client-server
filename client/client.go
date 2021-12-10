80bf2d9a98233441ac96ec2056753730cc4e557b
Mohammad Tauchid <muhammadtauchid@gmail.com>
2021-12-10 10:04:53 +0700

    fixing input output

--- client/client.go
+++ client/client.go
@@ -8,6 +8,15 @@ import (
         "strings"
 )
 
+var text string
+
+func getInput(c net.Conn) {
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Print(">> ")
+        text, _ = reader.ReadString('\n')
+        fmt.Fprintf(c, text+"\n")
+}
+
 func main () {
         arguments := os.Args
         if len(arguments) == 1 {
@@ -23,16 +32,21 @@ func main () {
         }
 
         for {
-                reader := bufio.NewReader(os.Stdin)
-                fmt.Print(">> ")
-                text, _ := reader.ReadString('\n')
-                fmt.Fprintf(c, text+"\n")
+                // reader := bufio.NewReader(os.Stdin)
+                // fmt.Print(">> ")
+                // text, _ = reader.ReadString('\n')
+                // fmt.Fprintf(c, text+"\n")
+                go getInput(c)
 
                 message, _ := bufio.NewReader(c).ReadString('\n')
-                fmt.Print("->: " + message)
                 if strings.TrimSpace(strings.ToUpper(string(text))) == "EXIT" {
                         fmt.Println("TCP client exiting...")
                         return
+                }
+                if message != c.LocalAddr().String() + "\n" {
+                        fmt.Print(message)
+                } else {        
+                        continue
                 }
         }
 }
